e0987cf1314f9c9994602f3beb82887948d074d6
John Biddulph <john.mbiddulph@gmail.com>
2020-03-14 19:53:33 +0000

    removed register

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -40,14 +40,7 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+
                         @else
                             <li class="mr-2">
                                 <a href="{{route('settings')}}">
